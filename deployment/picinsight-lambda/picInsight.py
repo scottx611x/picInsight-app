8e1459bafe74302a599b82f156cb9e0341a7d742
Scott Ouellette <scottx611x@gmail.com>
2018-04-28 10:34:30 -0400

    Update lambda function to gather more inforation about uploaded images

--- deployment/picinsight-lambda/picInsight.py
+++ deployment/picinsight-lambda/picInsight.py
@@ -1,34 +1,66 @@
 from __future__ import print_function
 
 import boto3
+import json
 
-print('Loading function')
 
-s3 = boto3.client('s3')
+class RekognitionAggregator(object):
+
+    def __init__(self, s3_event, region="us-east-1"):
+        self.s3_client = boto3.client('s3')
+        self.rekognition_client = boto3.client("rekognition", region)
+
+        uploaded_object_key = s3_event['object']['key']
+        self.uploaded_image = {
+            "S3Object": {
+                "Bucket": s3_event['bucket']['name'],
+                "Name": uploaded_object_key,
+            }
+        }
+        self.processed_bucket = "pic-insight-processed"
+        self.result_object_key = "{}-results.json".format(uploaded_object_key)
+        
+        self.image_info = self._get_image_info()
+
+    def upload_results(self):
+        results = bytes(
+            json.dumps(self.image_info), 
+            'utf-8'
+        )
+        self.s3_client.put_object(
+            Key=self.result_object_key,
+            Body=results,
+            Bucket=self.processed_bucket
+        )
+
+    def _get_image_info(self):
+        image_info = {
+            "celebrities": self._recognize_celebrities(),
+            "labels": self._detect_labels(),
+            "faces": self._detect_faces()
+        }
+        print(image_info)
+        return image_info
+
+    def _detect_labels(self, max_labels=7, min_confidence=95):
+        return self.rekognition_client.detect_labels(
+            Image=self.uploaded_image,
+            MaxLabels=max_labels,
+            MinConfidence=min_confidence,
+        )
+
+    def _detect_faces(self):
+        return self.rekognition_client.detect_faces(
+            Image=self.uploaded_image,
+            Attributes=['ALL']
+        )
+
+    def _recognize_celebrities(self):
+        return self.rekognition_client.recognize_celebrities(
+            Image=self.uploaded_image)
 
 
 def lambda_handler(event, context):
-    # Get the object from the event and show its content type
-    bucket = event['Records'][0]['s3']['bucket']['name']
-    print(bucket)
-    key = event['Records'][0]['s3']['object']['key']
-    print(key)
-    detect_labels(bucket, key)
+    s3_event = event['Records'][0]['s3']
+    RekognitionAggregator(s3_event).upload_results()
 
-
-def detect_labels(bucket, key, max_labels=5,
-                  min_confidence=95, region="us-east-1"):
-    rekognition = boto3.client("rekognition", region)
-    response = rekognition.detect_labels(
-        Image={
-            "S3Object": {
-                "Bucket": bucket,
-                "Name": key,
-            }
-        },
-        MaxLabels=max_labels,
-        MinConfidence=min_confidence,
-    )
-    print(response["Labels"])
-    for label in response["Labels"]:
-        print("{Name} - {Confidence}%".format(**label))
