bd6cf62abd27cae14f30678d6977f5a3e2c36e8b
Scott Ouellette <scottx611x@gmail.com>
2018-04-28 11:29:04 -0400

    Add tests

--- deployment/picinsight-lambda/tests.py
+++ deployment/picinsight-lambda/tests.py
@@ -1,2 +1,69 @@
+import unittest
+import mock
+
+from picInsight import RekognitionAggregator
+
+
+class MockRekognitonClient(object):
+
+    def detect_labels(self, Image=None, MaxLabels=None, MinConfidence=None):
+        return {
+            'Labels': []
+        }
+
+    def detect_faces(self, Image=None, Attributes=None):
+        return {
+            'FaceDetails': []
+        }
+
+    def recognize_celebrities(self, Image=None):
+        return {
+            'CelebrityFaces': []
+        }
+
+
+class MockS3Client(object):
+
+    def put_object(self, Key=None, Body=None, Bucket=None):
+        return None
+
+
+class RekognitionAggregatorTests(unittest.TestCase):
+
+    def setUp(self):
+        self.rekognition_mock = mock.patch(
+            "picInsight.RekognitionAggregator.rekognition_client",
+            MockRekognitonClient()
+        )
+        self.s3_mock = mock.patch(
+            "picInsight.RekognitionAggregator.s3_client",
+            MockS3Client()
+        )
+        self.rekognition_mock.start()
+        self.s3_mock.start()
+
+        s3_event = {
+            "object": {"key": "test.jpg"},
+            "bucket": {"name": "test-upload"}
+        }
+
+        with mock.patch("boto3.client"):
+        	self.r_aggregator = RekognitionAggregator(s3_event)
+
+    def tearDown(self):
+        mock.patch.stopall()
+
+    def test_upload_results(self):
+    	self.r_aggregator.upload_results()
+
+    	self.assertEqual(
+    		self.r_aggregator.image_info,
+			{
+				'celebrities': {'CelebrityFaces': []}, 
+				'labels': {'Labels': []}, 
+				'faces': {'FaceDetails': []}
+			}
+    	)
+
 if __name__ == '__main__':
     unittest.main()
