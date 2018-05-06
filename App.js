951b866cb14183af9dcde1cab49ae1a88d2d696b
Scott Ouellette <scottx611x@gmail.com>
2018-05-06 13:09:56 -0400

    Use amplyfiy to configure aws auth/storage

--- App.js
+++ App.js
@@ -1,6 +1,23 @@
 import React from 'react';
 import { Home, UploadPicture, ViewResults } from './components'
 import { createStackNavigator } from 'react-navigation';
+import Amplify, { Auth } from "aws-amplify";
+import awsConfig from './aws_data.json';
+
+Amplify.configure({
+    Auth: {
+      identityPoolId: awsConfig.identityPoolId, 
+      region: awsConfig.region
+    },
+    Storage: {
+      bucket: awsConfig.uploadBucket,
+      region: awsConfig.region
+    }
+});
+
+Auth.currentCredentials()
+.then(data => {console.log(data)})
+.catch(e => {console.log(e)});
 
 export default createStackNavigator(
   {
